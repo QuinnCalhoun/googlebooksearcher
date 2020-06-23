d326319ef6b47ca05d5f8795d656bfd1753a84c2
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-06-23 14:23:56 -0700

    fixing pages

---
+++ client/build/precache-manifest.2d1fc5d83391f4df25f20bc0e8041f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6da8b570670dae92440",
+    "url": "/static/css/main.ba7ba5fe.chunk.css"
+  },
+  {
+    "revision": "b6da8b570670dae92440",
+    "url": "/static/js/main.4a2f81b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca8176bbe395f0d059ae",
+    "url": "/static/js/2.f3cfacb6.chunk.js"
+  },
+  {
+    "revision": "3ba190ac49cccd1b867792894f275778",
+    "url": "/index.html"
+  }
+];
